f9c96ccd46b84134d0e8866a2684756bba1bae0e
C# Shark <stickyamp@hotmail.com>
2025-11-03 23:03:17 +0100

    default black mode

--- src/app/shared/services/config.service.ts
+++ src/app/shared/services/config.service.ts
@@ -1,46 +1,46 @@
-import { Injectable } from '@angular/core';
-import { Preferences } from '@capacitor/preferences';
+import { Injectable } from "@angular/core";
+import { Preferences } from "@capacitor/preferences";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ConfigService {
   constructor() {
     setTimeout(async () => {
       if (await this.isDarkMode()) {
-        console.log("manuXX aa")
+        console.log("manuXX aa");
         this.enableDarkMode();
-      }
-      else {
-        console.log("manuXX bb")
+      } else {
+        console.log("manuXX bb");
         this.enableLightMode();
       }
-
     }, 0);
-
   }
 
   public enableDarkMode() {
-    console.log("manuXX", { ...document.documentElement.classList })
-    document.documentElement.classList.remove('ion-palette-light');
-    document.documentElement.classList.add('ion-palette-dark');
+    console.log("manuXX", { ...document.documentElement.classList });
+    document.documentElement.classList.remove("ion-palette-light");
+    document.documentElement.classList.add("ion-palette-dark");
   }
 
   public enableLightMode() {
-    document.documentElement.classList.remove('ion-palette-dark');
-    document.documentElement.classList.add('ion-palette-light');
-
+    document.documentElement.classList.remove("ion-palette-dark");
+    document.documentElement.classList.add("ion-palette-light");
   }
 
   public async saveDarkModePreference(enabled: boolean): Promise<void> {
     await Preferences.set({
-      key: 'darkMode',
-      value: enabled.toString()
+      key: "darkMode",
+      value: enabled.toString(),
     });
   }
 
   public async isDarkMode(): Promise<boolean> {
-    const result = await Preferences.get({ key: 'darkMode' });
-    return result.value === 'true';
+    const result = await Preferences.get({ key: "darkMode" });
+    return (
+      result.value === "true" ||
+      result.value === undefined ||
+      result.value === null
+    );
   }
 }
