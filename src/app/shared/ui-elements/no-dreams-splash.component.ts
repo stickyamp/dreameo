fb3343381ddbc6f974d301908b79421283fb7328
C# Shark <stickyamp@hotmail.com>
2025-11-05 19:04:55 +0100

    clean

--- src/app/shared/ui-elements/no-dreams-splash.component.ts
+++ src/app/shared/ui-elements/no-dreams-splash.component.ts
@@ -1,21 +1,26 @@
 // Component TypeScript (no-dreams.component.ts)
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
-import { CommonModule } from '@angular/common';
-import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
-import { HttpClientModule } from '@angular/common/http';
-import { LoggerService } from '../services/log.service';
+import { Component, Input } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
+import { CommonModule } from "@angular/common";
+import { TranslateDirective, TranslatePipe } from "@ngx-translate/core";
+import { HttpClientModule } from "@angular/common/http";
+import { LoggerService } from "../services/log.service";
 
 @Component({
-    selector: 'app-no-dreams',
-    standalone: true,
-    imports: [IonicModule, CommonModule, HttpClientModule, TranslatePipe,
-        TranslateDirective],
-    templateUrl: './no-dreams-splash.component.html',
-    styleUrls: ['./no-dreams-splash.component.scss']
+  selector: "app-no-dreams",
+  standalone: true,
+  imports: [
+    IonicModule,
+    CommonModule,
+    HttpClientModule,
+    TranslatePipe,
+    TranslateDirective,
+  ],
+  templateUrl: "./no-dreams-splash.component.html",
+  styleUrls: ["./no-dreams-splash.component.scss"],
 })
 export class NoDreamsComponent {
-    constructor() { }
-    ngOnInit() {
-    }
+  @Input() isDaily: boolean = false;
+  constructor() {}
+  ngOnInit() {}
 }
